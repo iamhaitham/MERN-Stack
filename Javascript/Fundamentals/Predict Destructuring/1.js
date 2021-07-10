993f49782cf219a66e3fbf03f472630e542d7a06
iamhaitham <iamhaitham2011@gmail.com>
2021-07-10 22:28:13 +0200

    Added Predict Destructuring Assignment

---
+++ Javascript/Fundamentals/Predict Destructuring/1.js
@@ -0,0 +1,12 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [randomCar] = cars
+const [, otherRandomCar] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+//Output:
+/*
+Tesla
+Mercedes
+*/
---
+++ Javascript/Fundamentals/Predict Destructuring/2.js
@@ -0,0 +1,14 @@
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+//Output:
+/*
+ERROR
+*/
