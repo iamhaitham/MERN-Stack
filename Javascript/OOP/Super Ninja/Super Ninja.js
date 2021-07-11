667786ed51015c93348d64021a04d244f2f32f91
iamhaitham <iamhaitham2011@gmail.com>
2021-07-11 16:02:44 +0200

    Added Super Ninja Assignment

---
+++ Javascript/OOP/Super Ninja/Super Ninja.js
@@ -0,0 +1,34 @@
+class Ninja {
+    constructor(name, health) {
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName() {
+        console.log("Name: " + this.name);
+    }
+    showStats() {
+        console.log(`Name:  + ${this.name} +  Strength:  + ${this.strength} +  Speed:  + ${this.speed} +  Health:  + ${this.health}`);
+    }
+    drinkWater() {
+        this.health += 10;
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name, 200);
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom() {
+        super.drinkWater();
+        console.log(`What one programmer can do in one month, two programmers can do in two months.`);
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
