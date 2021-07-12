64935e340a7220df954eb79107d8e2ca427731ca
iamhaitham <iamhaitham2011@gmail.com>
2021-07-12 10:55:17 +0200

    Created Function Programming directory and added Object Master Assignment

---
+++ Javascript/Functional Programming/Object Master/Object Master.js
@@ -0,0 +1,65 @@
+const pokémon = Object.freeze([
+    { "id": 1, "name": "Bulbasaur", "types": ["poison", "grass"] },
+    { "id": 5, "name": "Charmeleon", "types": ["fire"] },
+    { "id": 9, "name": "Blastoise", "types": ["water"] },
+    { "id": 12, "name": "Butterfree", "types": ["bug", "flying"] },
+    { "id": 16, "name": "Pidgey", "types": ["normal", "flying"] },
+    { "id": 23, "name": "Ekans", "types": ["poison"] },
+    { "id": 24, "name": "Arbok", "types": ["poison"] },
+    { "id": 25, "name": "Pikachu", "types": ["electric"] },
+    { "id": 37, "name": "Vulpix", "types": ["fire"] },
+    { "id": 52, "name": "Meowth", "types": ["normal"] },
+    { "id": 63, "name": "Abra", "types": ["psychic"] },
+    { "id": 67, "name": "Machamp", "types": ["fighting"] },
+    { "id": 72, "name": "Tentacool", "types": ["water", "poison"] },
+    { "id": 74, "name": "Geodude", "types": ["rock", "ground"] },
+    { "id": 87, "name": "Dewgong", "types": ["water", "ice"] },
+    { "id": 98, "name": "Krabby", "types": ["water"] },
+    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+    { "id": 122, "name": "Mr. Mime", "types": ["psychic"] },
+    { "id": 133, "name": "Eevee", "types": ["normal"] },
+    { "id": 144, "name": "Articuno", "types": ["ice", "flying"] },
+    { "id": 145, "name": "Zapdos", "types": ["electric", "flying"] },
+    { "id": 146, "name": "Moltres", "types": ["fire", "flying"] },
+    { "id": 148, "name": "Dragonair", "types": ["dragon"] }
+]);
+
+// Example 1
+const pok = [...pokémon].filter(val => val.name.startsWith('B'));
+console.log(pok);
+
+// Example 2
+const ids = [...pokémon].map(val => val.id);
+console.log(ids);
+
+// Question 1
+const q1 = [...pokémon].filter(val => val.id % 3 == 0);
+console.log(q1);
+
+// Question 2
+const q2 = [...pokémon].filter(val => val.types.includes("fire"));
+console.log(q2);
+
+// Question 3
+const q3 = [...pokémon].filter(val => val.types.length > 1);
+console.log(q3);
+
+// Question 4
+const q4 = [...pokémon].map(val => val.name);
+console.log(q4);
+
+// Question 5
+const q5 = [...pokémon].filter(val => val.id > 99).map(x => x.name);
+console.log(q5);
+
+// Question 6
+const q6 = [...pokémon].filter(val => val.types.length == 1 && val.types.includes("poison")).map(x => x.name);
+console.log(q6);
+
+// Question 7
+const q7 = [...pokémon].filter(val => val.types.length > 1 && val.types[1] == "flying").map(x => x.types[0]);
+console.log(q7);
+
+// Question 8
+const q8 = [...pokémon].filter(val => val.types.includes("normal")).length;
+console.log(q8);
