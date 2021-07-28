10da329d2c417d0064fb831fdc16c3ccf60ecf43
iamhaitham <iamhaitham2011@gmail.com>
2021-07-28 15:40:32 +0200

    Added validations for the assignment

--- Full Stack MERN/Full Stack MERN/product-manager-part-1/server/models/product.model.js
+++ Full Stack MERN/Full Stack MERN/product-manager-part-1/server/models/product.model.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
-    title: { type: String },
-    price: { type: Number },
-    description: { type: String }
+    title: {
+        type: String,
+        required:[true,"Product title is mandatory!"]  
+    },
+    price: {
+        type: Number,
+        required:[true,"Product price is mandatory!"]  
+    },
+    description: { 
+        type: String,
+        required:[true,"Product description is mandatory"]
+    }
 }, { timestamps: true });
 
 module.exports.Product = mongoose.model('Product', ProductSchema);
