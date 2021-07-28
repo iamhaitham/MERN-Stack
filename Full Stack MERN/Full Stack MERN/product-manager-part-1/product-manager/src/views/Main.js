882760767466a293d41dad0173186041c1a8bd8d
iamhaitham <iamhaitham2011@gmail.com>
2021-07-28 15:32:04 +0200

    Created Full Stack MERN directory, another one with the same name and Advanced MERN directory. I also added Product Manager Part 1 Assignment

---
+++ Full Stack MERN/Full Stack MERN/product-manager-part-1/product-manager/src/views/Main.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import ProductManager from '../components/ProductManager';
+const Main = () => {
+    return (
+        <div>
+           <ProductManager/>
+        </div>
+    )
+}
+export default Main;
