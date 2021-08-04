d8ed0d1365184a71574e9360d5bc492373f8c027
iamhaitham <iamhaitham2011@gmail.com>
2021-08-04 15:07:29 +0200

    Created Socket.io and creating the handshake directories, and added Creating The Handshake assignment

---
+++ Socket.io/creating-the-handshake/creating-the-handshake/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Chat from '../src/components/Chat.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Chat/>
+    </div>
+  );
+}
+
+export default App;
